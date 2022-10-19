35cb8e9961f7d0e08a601a445702803a5d78f09b
MichaelHo <michelo851a1203@gmail.com>
2022-10-20 00:28:12 +0800

    finish Task1, Task2 python # 20221019001
    	修改：     Task1.py
    	修改：     Task2.py

--- Task1.py
+++ Task1.py
@@ -11,6 +11,24 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
+resultList:list[str] = []
+
+for textItem in texts: 
+    if textItem[0] not in resultList:
+        resultList.append(textItem[0])
+    if textItem[1] not in resultList:
+        resultList.append(textItem[1])
+
+for callItem in calls:
+    if callItem[0] not in resultList:
+        resultList.append(callItem[0])
+    if callItem[1] not in resultList:
+        resultList.append(callItem[1])
+
+
+print("There are {count} different telephone numbers in the records.".format(
+    count=len(resultList)
+    ))
 
 """
 TASK 1:
--- Task2.py
+++ Task2.py
@@ -11,6 +11,40 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
+
+# class PhoneClass:
+#     def __init__(self, name: str, duration: int):
+#         self.name = name
+#         self.duration = duration
+#
+# resultList:list[PhoneClass] = []
+#
+# sendingTelephoneList = list(map(lambda sending: PhoneClass(sending[0], int(sending[3])), calls))
+# receiveTelephoneList = list(map(lambda receiving: PhoneClass(receiving[1], int(receiving[3])), calls))
+# allPhoneList = sendingTelephoneList + receiveTelephoneList
+resultObject = {}
+
+for item in calls:
+    if item[0] in resultObject.keys():
+        resultObject[item[0]] += int(item[3])
+    else:
+        resultObject[item[0]] = int(item[3])
+
+    if item[1] in resultObject.keys():
+        resultObject[item[1]] += int(item[3])
+    else:
+        resultObject[item[1]] = int(item[3])
+
+(resultKey, resultValue) = "", 0
+for dictionayKey in resultObject.keys():
+    if resultObject[dictionayKey] > resultValue:
+        (resultKey, resultValue) = (dictionayKey,resultObject[dictionayKey])
+
+print("{telePhoneName} spent the longest time, {totalTime} seconds, on the phone during September 2016.".format(
+    telePhoneName=resultKey,
+    totalTime=resultValue,
+    ))
+
 """
 TASK 2: Which telephone number spent the longest time on the phone
 during the period? Don't forget that time spent answering a call is
